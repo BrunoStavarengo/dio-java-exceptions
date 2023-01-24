f2ed413895ab0a17a7ab0d7edf7912b618b45876
Bruno <brunostavarengo12@gmail.com>
2023-01-24 13:02:45 -0300

    Exemplo unchecked exception

---
+++ ExemploUncheckedException/src/Main.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean continueLooping = true;
+
+        do{
+            String a = JOptionPane.showInputDialog("Numerador: ");
+            String b = JOptionPane.showInputDialog("Denominador: ");
+
+            try {
+                int resultado = dividir(Integer.parseInt(a), Integer.parseInt(b));
+                System.out.println("Resultado: " + resultado);
+                continueLooping = false;
+            } catch (NumberFormatException e){
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Entrada invalida, digite um numero inteiro!" + e.getMessage());
+            }catch (ArithmeticException e) {
+                e.printStackTrace();
+                JOptionPane.showMessageDialog(null, "Impossivel dividir um numero por 0.");
+            }finally {
+                System.out.println("Chegou no finally");
+            }
+        }while (continueLooping);
+
+
+        System.out.println("O codigo continua...");
+    }
+
+    public static int dividir(int a, int b){
+        return a/b;
+    }
+}
